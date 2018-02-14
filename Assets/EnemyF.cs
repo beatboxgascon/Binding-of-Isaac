45a3082309f906c8b49676df491b0e5ec1b10484
beatboxgascon <32103615+beatboxgascon@users.noreply.github.com>
2018-02-14 17:44:15 +0100

    Seguimiento enemigo al personaje

--- Assets/EnemyF.cs
+++ Assets/EnemyF.cs
@@ -7,23 +7,25 @@ public class EnemyF : MonoBehaviour
     public float speed = 3f;
 
 
-    // Use this for initialization
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
+
+        //rotate to look at the player
         transform.LookAt(target.position);
-        transform.Rotate(new Vector3(0, -90, 0), Space.Self);//correcting the original rotation
+        transform.Rotate(new Vector3(0, -90, 0));//correcting the original rotation
+
 
         //move towards the player
         if (Vector3.Distance(transform.position, target.position) > 1f)
         {//move if distance from target is greater than 1
             transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
         }
+
     }
 
 
