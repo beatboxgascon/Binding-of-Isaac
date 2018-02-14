8f6d35fd7c8ecbb9ea5a8b523dc5f6bfcbf0b754
beatboxgascon <32103615+beatboxgascon@users.noreply.github.com>
2018-02-14 18:14:43 +0100

    Colision con enemigo

--- Assets/EnemyF.cs
+++ Assets/EnemyF.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class EnemyF : MonoBehaviour
 {
     public Transform target;//set target from inspector instead of looking in Update
-    public float speed = 3f;
+    public float speed = 1f;
 
 
     void Start()
@@ -14,18 +14,7 @@ public class EnemyF : MonoBehaviour
 
     void Update()
     {
-
-        //rotate to look at the player
-        transform.LookAt(target.position);
-        transform.Rotate(new Vector3(0, -90, 0));//correcting the original rotation
-
-
-        //move towards the player
-        if (Vector3.Distance(transform.position, target.position) > 1f)
-        {//move if distance from target is greater than 1
-            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
-        }
-
+        transform.position += (target.transform.position - transform.position).normalized * speed * Time.deltaTime;
     }
 
 
