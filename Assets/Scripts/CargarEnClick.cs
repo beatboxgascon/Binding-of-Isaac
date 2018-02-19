8c10e0a49719c8f46e78bc07443ba6e1e80b6be0
MarioGit333 <32578491+MarioGit333@users.noreply.github.com>
2018-02-19 17:56:58 +0100

    Menu Principal
    menu principal carga juego

---
+++ Assets/Scripts/CargarEnClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CargarEnClick : MonoBehaviour
+{
+
+    public void CargarPorIndice(int indice)
+    {
+        SceneManager.LoadScene(indice);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
