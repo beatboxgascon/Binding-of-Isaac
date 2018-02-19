f7d8972b04ac441660bad3551f0cb61c6689cf63
beatboxgascon <32103615+beatboxgascon@users.noreply.github.com>
2018-02-19 18:00:39 +0100

    Poco cambio

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
-{   
-
+{
+    public static GameObject[] myObjects;
+    //I used this to keep track of the number of objects I spawned in the scene.
     // Use this for initialization
     void Start()
     {
-       
+        myObjects = Resources.LoadAll<GameObject>("Objetos");
+        int whichItem = 0;
+
+
+        GameObject myObj = Instantiate(myObjects[whichItem]) as GameObject;
+        myObj.transform.position = transform.position;
     }
 
     // Update is called once per frame
